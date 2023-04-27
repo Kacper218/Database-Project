284ea44521350df2f38ffd93fe8d99ba745bad0d
Kacper2188 <kacper.przybyszewskiiii@gmail.com>
2023-04-27 18:37:48 +0200

    Added a mapping to a Clients table

---
+++ Projekt_bazodanowy/Models/Klienci.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_bazodanowy.Models
+{
+    public class Klienci
+    {
+        public virtual int IDKlienta { get; set; }
+        public virtual string ImieNazwisko { get; set; }
+        public virtual string NazwaFirmy { get; set; }
+        public virtual string Email { get; set; }
+    }
+}
