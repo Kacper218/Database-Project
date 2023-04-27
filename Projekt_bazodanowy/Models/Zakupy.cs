35bc416cc363fff70ab3b3cf8b2b9c5401973b54
Kacper2188 <kacper.przybyszewskiiii@gmail.com>
2023-04-27 19:49:16 +0200

    Added a mapping to a Shopping table

---
+++ Projekt_bazodanowy/Models/Zakupy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_bazodanowy.Models
+{
+    public class Zakupy
+    {
+        public virtual int IDZakupu { get; set; }
+        public virtual string IDDokumentu { get; set; }
+        public virtual int IDProduktu { get; set; }
+        public virtual int Ilosc { get; set; }
+        public virtual double CenaZakupu { get; set; }
+    }
+}
