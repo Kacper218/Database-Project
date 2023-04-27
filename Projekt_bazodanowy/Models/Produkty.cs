f0dc8ddd1eb5b3be52dc27fd5206ac23a6f975a0
Kacper2188 <kacper.przybyszewskiiii@gmail.com>
2023-04-27 18:59:45 +0200

    Added a mapping to a Products table

---
+++ Projekt_bazodanowy/Models/Produkty.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_bazodanowy.Models
+{
+    public class Produkty
+    {
+        public virtual int IDProduktu { get; set; }
+        public virtual string Nazwa { get; set; }   
+        public virtual string CenaAktualna { get; set; }
+        public virtual int Dostepnosc { get; set; } 
+    }
+}
